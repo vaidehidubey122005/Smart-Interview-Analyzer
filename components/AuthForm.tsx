a91b1bbbb5269db79a1eb12ab19fbba13dca32bf
Vaidehi dubey <vaidehidubey122005@users.noreply.github.com>
2025-05-24 20:02:32 +0530

    "commit"

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -48,10 +48,12 @@ const AuthForm = ({type}:{type: FormType}) => {
             </form>
     </Form>
     <p className="text-center">
-          {isSignIn ? 'No account yet?' : 'Have an account already?'}
-          <Link href={!isSignIn ? '/sign-in' : '/sign-up'}
-            className="font-bold text-user-primary ml-1">
-            {!isSignIn ? "Sign in" : 'Sign up'}
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign In" : "Sign Up"}
           </Link>
         </p>
     </div>
