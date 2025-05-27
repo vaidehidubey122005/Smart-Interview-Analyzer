c22161f900cf6ff91679765ddc5e324202d78cda
Vaidehi dubey <vaidehidubey122005@users.noreply.github.com>
2025-05-27 21:13:14 +0530

    "commit"

--- app/(auth)/sign-up/page.tsx
+++
@@ -1,6 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-
-const Page = () => {
-    return <AuthForm type="sign-up" />;
-}
-export default Page;
--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Page=()=>{
-  return(
-    <div>Home Page</div>
-  )
-}
-export default Page
